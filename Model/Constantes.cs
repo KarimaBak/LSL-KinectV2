adec7317426500a3221210081cbcd5ab4f34e0b7
Benoit Prigent <benoitp35.bp@gmail.com>
2020-06-16 16:46:44 +0200

    Project hierarchy

--- Model/Constantes.cs
+++ Model/Constantes.cs
@@ -5,7 +5,7 @@ namespace LSL_Kinect
     public static class Constantes
     {
         #region Constants
-
+        //TODO Move to ressources file or smth
         public static readonly double DPI = 96.0;
         public static readonly PixelFormat FORMAT = PixelFormats.Bgr32;
         public static readonly int BYTES_PER_PIXEL = (FORMAT.BitsPerPixel + 7) / 8;
